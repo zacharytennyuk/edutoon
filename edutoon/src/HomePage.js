8796283ea7e3219c332e159d87a96bedf62297b8
Zachary Tenn Yuk <ztennyuk@gmail.com>
2024-02-06 16:41:52 -0500

    Added React

---
+++ edutoon/src/HomePage.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function HomePage() {
+  return <div className="HomePage">
+  <h3>EduToon: Making academic research more accessible.</h3>
+  <h4>Welcome to EduToon! This tool uses generative AI to turn research papers into informational comics.
+      Please upload the paper you want to turn into a comic (.pdf or .docx).
+  </h4>
+  <ul>
+      <li>Upload a research pdf.</li>
+      <li>Generative AI will summarize it.</li>
+      <li>Enjoy the comic!</li>
+  </ul>
+  {/* <a href="https://platform.openai.com/docs/models/gpt-4-and-gpt-4-turbo">Using GPT-4 OpenAI API</a>
+  <img src=""></img> */}
+  
+  <form action="index.html" method="GET">
+      <p>Add Research PDF</p>
+      <input type="file" id="myFile" name="filename"/>
+      <input type="submit" value="Submit"/>
+  </form>
+  
+  <form action="index.html" method="GET">
+      <p>Number of Panels</p>
+      <input type="number" id="panelnumbers" name="numbers"/>
+      <input type="submit" value="Submit"/>
+  </form>
+  
+  <form action="index.html" method="GET">
+      <p>Comic Scenario</p>
+      <input type="text" id="panelscene" name="texts"/>
+      <input type="submit" value="Submit"/>
+  </form>
+  </div>
+}
+
+export default HomePage;
