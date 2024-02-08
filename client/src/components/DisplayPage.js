344da2b34fa585a8b2966b261cf5f792a34cbc96
Zachary Tenn Yuk <ztennyuk@gmail.com>
2024-02-08 16:16:21 -0500

    removed extraneous files

--- client/src/components/DisplayPage.js
+++ client/src/components/DisplayPage.js
@@ -10,6 +10,7 @@ function DisplayPage({history}) {
         <img src="https://cdn.pixabay.com/photo/2014/11/16/23/16/upset-534103_1280.jpg"
         width="640"
         height="480"
+        alt="Generated comic panel (placeholder)" 
         />
         <br></br>
         <button onClick={() => navigate('/')}>Restart</button>
